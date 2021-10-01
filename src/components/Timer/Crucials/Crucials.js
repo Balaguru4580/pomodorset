9fcdc72ef2bfd506da50da54dc865e444aedba98
Kapil-Kawathekar <kapilkawathekar8@gmail.com>
2021-10-01 10:16:28 +0530

    bugFix and Feature Improvements

--- src/components/Timer/Crucials/Crucials.js
+++ src/components/Timer/Crucials/Crucials.js
@@ -1,35 +1,58 @@
-import React, {useState} from "react";
+import React, { Fragment, useState } from "react";
 import Button from "@material-ui/core/Button";
 import { Input } from "@material-ui/core";
 import styles from "./Crucials.module.css";
 export default function Crucials({ timerCommand }) {
+  const [Hdata, setHData] = useState("");
+  const [Mdata, setMData] = useState("");
+  let data = [];
 
-    const [Hdata, setHData] = useState(0);
-    const [Mdata, setMData] = useState(0);
-    let data = [];
+  function timerStart() {
+    //For Testing Purpose
 
-    function timerStart(){
-        if(Hdata === 0 && Mdata === 0){
-            console.log("Minimum Timer Initiated");
-            setHData((minH)=> minH = 0);
-            setMData((minM)=> minM = 15);
-        }
-        data[0] = 0;
-        data[1] = Mdata;
-        data[2] = Hdata;
-        timerCommand(data)
+    // if (Hdata === 0 && Mdata === 0) {
+    //   console.log("Minimum Timer Initiated");
+    //   setHData((minH) => (minH = 0));
+    //   setMData((minM) => (minM = 15));
+    // }
+
+    //Validation of Minute and Hour Data
+    if (Mdata === "" || Hdata === "" || Mdata < 0 || Hdata < 0) {
+      alert("Please enter valid values into the Minutes and Hours field");
+      setHData("");
+      setMData("");
+      return;
     }
+    data[0] = 0;
+    data[1] = Mdata || "0";
+    data[2] = Hdata || "0";
+    setHData("");
+    setMData("");
+    timerCommand(data);
+  }
 
-
-    return (<div>
-        <Button variant="contained" color="primary" onClick={timerStart}> Start Timer</Button>
-        <form className={styles.HTM} onSubmit={timerStart}>
-            <Input type="number" onChange={e => setHData(e.target.value) } id="outlined-basic" placeholder="Hours(H)" ></Input>
-            <Input type="number" id="outlined-basic"  onChange={e => setMData(e.target.value)} placeholder="Minutes(M)" ></Input>
-        </form>
-    </div>
-
-    )
-
+  return (
+    <Fragment>
+      <Button variant="contained" color="primary" onClick={timerStart}>
+        {" "}
+        Start Timer
+      </Button>
+      <form className={styles.HTM} onSubmit={timerStart}>
+        <Input
+          type="number"
+          value={Hdata}
+          onChange={(e) => setHData(e.target.value)}
+          id="outlined-basic"
+          placeholder="Hours(H)"
+        ></Input>
+        <Input
+          type="number"
+          id="outlined-basic"
+          value={Mdata}
+          onChange={(e) => setMData(e.target.value)}
+          placeholder="Minutes(M)"
+        ></Input>
+      </form>
+    </Fragment>
+  );
 }
-
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import styles from './Timer.module.scss';
+import styles from "./Timer.module.scss";
 import Crucials from "./Crucials/Crucials";
 export default function Timer() {
-
   let Seconds = 0;
   let Minutes = 0;
   let Hours = 5;
@@ -10,65 +9,66 @@ export default function Timer() {
   const [Mtimer, setMTimer] = React.useState(Minutes);
   const [Htimer, setHTimer] = React.useState(Hours);
 
-  const [data, setData] = useState('');
+  const [data, setData] = useState([]);
 
   const id = React.useRef(null);
   const clear = () => {
-    window.clearInterval(id.current)
-  }
-
+    window.clearInterval(id.current);
+  };
 
   React.useEffect(() => {
     id.current = window.setInterval(() => {
-      setSTimer((time) => time - 1)
-    }, 1000)
+      setSTimer((time) => time - 1);
+    }, 1000);
     //NOTNEEDED return ()=>clear();
-
-  }, []) //Seconds Tick Down Mechanism 
+  }, []); //Seconds Tick Down Mechanism
 
   React.useEffect(() => {
     //OVERFLOW prevention
     if (Htimer > 60 || Mtimer > 60 || Stimer > 60) {
-      setSTimer((Stime) => Stime = 59)
-      setMTimer((Mtime) => Mtime = 59)
-      setHTimer((Htime) => Htime = 60)
-
+      setSTimer((Stime) => (Stime = 59));
+      setMTimer((Mtime) => (Mtime = 59));
+      setHTimer((Htime) => (Htime = 60));
     }
     // Hours to minutes drop
     else if (Mtimer === 0 && Htimer !== 0 && Stimer === 0) {
-      setMTimer((Mtime) => Mtime + 60)
-      setHTimer((Htime) => Htime - 1)
+      setMTimer((Mtime) => Mtime + 60);
+      setHTimer((Htime) => Htime - 1);
     }
     // Minutes to Seconds Drop
     else if (Stimer === 0 && Mtimer !== 0) {
-      setSTimer((Stime) => Stime + 59)
-      setMTimer((Mtime) => Mtime - 1)
+      setSTimer((Stime) => Stime + 59);
+      setMTimer((Mtime) => Mtime - 1);
     }
     //Timer Complete Event
     else if (Stimer === 0) {
-      console.log("COMPLETE")
-      clear()
+      console.log("COMPLETE");
+      // id.current = null;
+      clear();
     }
-  }, [Stimer, Mtimer, Htimer])
+  }, [Stimer, Mtimer, Htimer]);
 
   const timerCommand = (timedata) => {
-    setData(timedata)
-    setSTimer((s) => s = data[0])
-    setMTimer((s) => s = data[1])
-    setHTimer((s) => s = data[2])
-    if (!id.current) {
-      id.current = window.setInterval(() => {
-        setSTimer((time) => time - 1)
-      }, 1000)
+    setData(timedata);
+    setSTimer((s) => (s = +timedata[0]));
+    setMTimer((s) => (s = +timedata[1]));
+    setHTimer((s) => (s = +timedata[2]));
+    if (id.current) {
+      //clearing the timer if any exist
+      clear();
+      if (+timedata[0] !== 0 || +timedata[1] !== 0 || +timedata[2] !== 0)
+        id.current = window.setInterval(() => {
+          setSTimer((time) => time - 1);
+        }, 1000);
     }
   };
 
   return (
-    <div id = "Clock"  className={styles.timerstyle}>
+    <div id="Clock" className={styles.timerstyle}>
       <h1 style={{ fontSize: 125 }}>
         {Htimer}:{Mtimer}:{Stimer}
       </h1>
-      <div id = "Controls" className={styles.crucials}>
+      <div id="Controls" className={styles.crucials}>
         <Crucials timerCommand={timerCommand} />
       </div>
     </div>
